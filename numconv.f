942d20e12bff7bae30651fc37681e35e53923f61
LauraPosner <lnp@andrew.cmu.edu>
2013-12-11 16:53:10 -0500

    Permit offline emissions size redistribution
    The previous version of numconv.f applied size distributions to sources
    online. These changes work with emissions files that had size
    distributions applied offline.

--- numconv.f
+++ numconv.f
@@ -12,24 +12,18 @@ c
 c
 c
 c     NUMCONV generates number concentrations from mass concentrations.
-c     During emission process, sulfate, crust, and carbonaceous particles
-c     are redistributed. It is because the emission inventries are only
-c     separated by fine and coarse modes. (Flat distribution within size
-c     section) The redistribution follows,
-c
-c     Sulfate: modified AEROCOM sulfate distribution
-c     Crust: Lee, Y-H's global emission from Marticorena and Bergamett (1995) 
-c     Carbonaceous: Figure 3-(b) on AE39 (2005) 4155-4166
+c     In this version, emissions, BC, IC, and top conditions were 
+c     redistributed offline. LNP 03282012
 c     
 c     Arguments:
 c     conc ; concentrations including aerosol and gas
-c     ncol ; a number of columes of whole grids
+c     ncol ; a number of columns of whole grids
 c     nrow ; a number of rows of whole grids
 c     nlay ; a number of layers of whole grids
 c     tmass ; total mass concentration before doing individual prcesses
-c     iflag ; 0 = initial condition, implement the organinc distribution
-c           ; 1 = Do not implement the initial condition
-c           ; 2 = called by emistrns. Accumulate number concentration
+c     iflag ; 0 = initial condition
+c           ; 1 = boundary condition
+c           ; 2 = emissions
 c
 c     Called by:
 c     CAMx
@@ -52,228 +46,11 @@ c
       integer iflag ! different depending on calling subroutines
       integer isund
       real mass ! [=] ug/m3, total mass of each size section
-      real mass2 ! [=] ug/m3, total mass for organic species
-      real mass3 ! [=] ug/m3, total mass for number concentratios
       real number ! [=] #/cm3, aerosol number concentration
       real meandp3 ! [=] a cube of mean diameter of particle
-c      real dist(14) !Carbonaceous redistribution
-      real dist(10) !Carbonaceous redistribution
-      real dist2(12), dist3(6) !Crust redistributions between 2.5 um 
-      real dist4(35), dist5(6) !Sulfate redistribution between 2.5 um
       dimension conc(ncol,nrow,nlay,nspc)
       dimension sconc(ncol,nrow,nlay,nspc)
 c
-c-----Distribute Organic Mass based on the following paper.
-c     Figure 3-(b) on AE 39 (2005) 4155-4166
-c     Organic Mass includes emissions below 40 nm.
-c     The species of organics correspond to the followings.
-c
-c     Portions of mass from 12th to 25th. The values below calculated in
-c     Excel file.
-c
-c      data dist /5.15e-5, 1.72e-4, 4.29e-4, 9.44e-4, 1.92e-3,
-c     &           3.84e-3, 1.24e-2, 4.39e-2, 9.06e-2, 1.21e-1,
-c     &           1.76e-1, 1.98e-1, 1.76e-1, 1.76e-1/
-
-c     Portions of mass from 9th to 18th. The values below calculated in
-c     Excel file.
-
-      data dist /1.70e-3, 3.32e-3, 7.70e-3, 1.73e-2, 4.29e-2,
-     &           7.49e-2, 1.39e-1, 2.00e-1, 2.28e-1, 2.86e-1/
-c
-c-----Distribution of crust
-c
-c     Portions of mass from 24th to 35th. The values below calculated in
-c     the excel file of "modified_crust.xls". (Less than 2.5 um)
-c
-      data dist2 /5.70e-7, 1.03e-5, 9.69e-5, 5.78e-4, 2.20e-3,
-     &           5.96e-3, 1.33e-2, 2.77e-2, 5.93e-2, 1.29e-1,
-     &           2.66e-1, 4.96e-1/
-c
-c     Portions of mass from 36th to 41th. The values below calculated in
-c     the excel file of "modified_crust.xls". (Bigger than 2.5 um)
-c
-      data dist3 /1.06e-1, 1.52e-1, 1.89e-1, 2.03e-1, 1.92e-1,
-     &           1.58e-1/
-c
-c-----Distribution of sulfate and etc
-c
-c     Portions of mass from 1st to 35th. The values below calculated in
-c     the excel file of "modified_crust.xls". (Less than 2.5 um)
-c
-      data dist4 /3.98e-15, 7.69e-14, 1.27e-12, 1.81e-11, 2.20e-10,
-     &           2.30e-9, 2.05e-8, 1.57e-7, 1.03e-6, 5.79e-6,
-     &           2.79e-5, 1.15e-4, 4.07e-4, 1.23e-3, 3.20e-3,
-     &           7.14e-3, 1.37e-2, 2.25e-2, 3.19e-2, 3.93e-2,
-     &           4.24e-2, 4.08e-2, 3.59e-2, 2.96e-2, 2.32e-2,
-     &           1.74e-2, 1.26e-2, 9.58e-3, 1.01e-2, 1.68e-2,
-     &           3.34e-2, 6.46e-2, 1.13e-1, 1.79e-1, 2.52e-1/
-c
-c     Portions of mass from 36th to 41th. The values below calculated in
-c     the excel file of "modified_crust.xls". (Bigger than 2.5 um)
-c
-      data dist5 /1.74e-1, 1.96e-1, 1.99e-1, 1.80e-1, 1.46e-1,
-     &           1.06e-1/
-c
-      k = 1 ! Apply only surface cells
-      do j = 1,nrow
-         do i = 1,ncol
-c
-c-----Collect Organic mass in the size sections from 18 to 25,
-c-----which corresponds from 40.6 nm to 258 nm.
-c
-cnotime            if ((time.ge.600).and.(time.lt.1800)) then !Only daytime
-               do iarspc = 5, 6 !from POC to KEC (See CAMx4.chemparam.6)
-                  mass2 = 0.
-                  do isec = 9, 18
-                     lmod = ngas + (iarspc-1)*nsec + isec
-                     if ((iflag.eq.0).or.(iflag.eq.1)) then
-                        mass2 = mass2 + conc(i,j,k,lmod)
-                        conc(i,j,k,lmod) = 0.
-                     else !for emission
-                        mass2 = mass2 + (conc(i,j,k,lmod)
-     &                     - sconc(i,j,k,lmod))
-                        conc(i,j,k,lmod) = sconc(i,j,k,lmod)
-                     endif
-                  enddo
-
-                  !Redistribute OM to the size bins from 9th (5.08 nm) 
-                  !to 18th (51.2 nm).
-                  do ii = 1,10
-                     isec = ii + 8
-                     lmod = ngas + (iarspc-1)*nsec + isec
-                     if ((iflag.eq.0).or.(iflag.eq.1)) then
-                        conc(i,j,k,lmod) = mass2 * dist(ii)
-                     else !for emission
-                        conc(i,j,k,lmod) = sconc(i,j,k,lmod) 
-     &                    + (mass2 * dist(ii))
-                     endif
-                  enddo
-               enddo
-cnotime            endif
-c
-c-----Collect crustal mass in the size sections from 1 to 35,
-c-----which corresponds from 0.8 nm to 2.6 um
-c
-            iarspc = 7 !CRST (See CAMx4.chemparam.6)
-            mass2 = 0.
-            do isec = 1, 35
-               lmod = ngas + (iarspc-1)*nsec + isec
-               if ((iflag.eq.0).or.(iflag.eq.1)) then
-                  mass2 = mass2 + conc(i,j,k,lmod)
-                  conc(i,j,k,lmod) = 0.
-               else !for emission
-                  mass2 = mass2 + (conc(i,j,k,lmod)
-     &               - sconc(i,j,k,lmod))
-                  conc(i,j,k,lmod) = sconc(i,j,k,lmod)
-               endif
-            enddo
-
-            !Redistribute crust to the size bins from 24th (163 nm) 
-            !to 35th (2.6 nm).
-            do ii = 1,12
-               isec = ii + 23
-               lmod = ngas + (iarspc-1)*nsec + isec
-               if ((iflag.eq.0).or.(iflag.eq.1)) then
-                  conc(i,j,k,lmod) = mass2 * dist2(ii)
-               else !for emission
-                  conc(i,j,k,lmod) = sconc(i,j,k,lmod) 
-     &              + (mass2 * dist2(ii))
-               endif
-            enddo
-c
-c     Collect crustal mass in the size sections from 36 to 41,
-c     which corresponds from 2.6 um to 10.4 um
-            mass2 = 0.
-            do isec = 36, 41
-               lmod = ngas + (iarspc-1)*nsec + isec
-               if ((iflag.eq.0).or.(iflag.eq.1)) then
-                  mass2 = mass2 + conc(i,j,k,lmod)
-                  conc(i,j,k,lmod) = 0.
-               else !for emission
-                  mass2 = mass2 + (conc(i,j,k,lmod)
-     &               - sconc(i,j,k,lmod))
-                  conc(i,j,k,lmod) = sconc(i,j,k,lmod)
-               endif
-            enddo
-
-            !Redistribute crust to the size bins from 36th (2.6 um) 
-            !to 41th (10.4 um).
-            do ii = 1,6
-               isec = ii + 35
-               lmod = ngas + (iarspc-1)*nsec + isec
-               if ((iflag.eq.0).or.(iflag.eq.1)) then
-                  conc(i,j,k,lmod) = mass2 * dist3(ii)
-               else !for emission
-                  conc(i,j,k,lmod) = sconc(i,j,k,lmod) 
-     &              + (mass2 * dist3(ii))
-               endif
-            enddo
-c
-c-----Collect sulfate and etc in the size sections from 1 to 35,
-c-----which corresponds from 0.8 nm to 2.6 um.
-c
-            do iarspc = 9, 13 !from PCL to PSO4 (See CAMx4.chemparam.6)
-               mass2 = 0.
-               do isec = 1, 35
-                  lmod = ngas + (iarspc-1)*nsec + isec
-                  if ((iflag.eq.0).or.(iflag.eq.1)) then
-                     mass2 = mass2 + conc(i,j,k,lmod)
-                     conc(i,j,k,lmod) = 0.
-                  else !for emission
-                     mass2 = mass2 + (conc(i,j,k,lmod)
-     &                  - sconc(i,j,k,lmod))
-                     conc(i,j,k,lmod) = sconc(i,j,k,lmod)
-                  endif
-               enddo
-
-               !Redistribute OM to the size bins from 1st (0.8 nm) 
-               !to 35th (2.6 um).
-               do ii = 1,35
-                  isec = ii
-                  lmod = ngas + (iarspc-1)*nsec + isec
-                  if ((iflag.eq.0).or.(iflag.eq.1)) then
-                     conc(i,j,k,lmod) = mass2 * dist4(ii)
-                  else !for emission
-                     conc(i,j,k,lmod) = sconc(i,j,k,lmod) 
-     &                 + (mass2 * dist4(ii))
-                  endif
-               enddo
-c
-c-----Collect sulfate and etc in the size sections from 36 to 41,
-c-----which corresponds from 2.6 um to 10.4 um.
-c
-               mass2 = 0.
-               do isec = 36, 41
-                  lmod = ngas + (iarspc-1)*nsec + isec
-                  if ((iflag.eq.0).or.(iflag.eq.1)) then
-                     mass2 = mass2 + conc(i,j,k,lmod)
-                     conc(i,j,k,lmod) = 0.
-                  else !for emission
-                     mass2 = mass2 + (conc(i,j,k,lmod)
-     &                  - sconc(i,j,k,lmod))
-                     conc(i,j,k,lmod) = sconc(i,j,k,lmod)
-                  endif
-               enddo
-
-              !Redistribute OM to the size bins from 36th (2.6 um) 
-              !to 41th (10.4 um).
-              do ii = 1,6
-                 isec = ii+35
-                 lmod = ngas + (iarspc-1)*nsec + isec
-                 if ((iflag.eq.0).or.(iflag.eq.1)) then
-                    conc(i,j,k,lmod) = mass2 * dist5(ii)
-                 else !for emission
-                    conc(i,j,k,lmod) = sconc(i,j,k,lmod) 
-     &                + (mass2 * dist5(ii))
-                 endif
-              enddo
-            enddo
-c
-c-------------------------------------------------------------------------
-c
-         enddo !ncol
-      enddo !nlow
 c
 c-----Generate number conc.
 c
@@ -318,7 +95,7 @@ c
       subroutine saveconc(conc,ncol,nrow,nlay,nspc,sconc)
 c
 c
-c     SAVECONC generates number concentrations from mass concentrations.
+c     SAVECONC saves mass concentrations before redistributing.
 c
 c     Arguments:
 c     conc ; concentrations including aerosol and gas
