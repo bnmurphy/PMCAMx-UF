f31a3dab5d3aa910dda81a8e796feeb9dc27a7dc
bnmurphy <benmurphy@alumni.cmu.edu>
2014-10-28 23:36:43 +0100

    Added runfiles

---
+++ runfiles/runshell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH -J PMCAMxUF_OMP
+#SBATCH -t 2-3:00:00
+#SBATCH --mem=4000
+#SBATCH -N 1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=benjamin.murphy@itm.su.se
+#SBATCH --constraint=vtune
+#
+# Run a single task in the foreground
+./run_days 
+#
+# Script ends here
